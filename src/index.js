61952ebd3d75d02dfd8e4f2ca1e29ebd03aca65a
Roberto Reyes <rreyes@connectiveit.com.mx>
2023-12-13 14:16:37 -0600

    v2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ApexChart from './ApexChart';
 import DonutChart from './DonutChart';
+import PyramidChart from './PyramidChart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
     <App />
     <ApexChart />
     <DonutChart />
+    <PyramidChart />
   </React.StrictMode>
 );
 
